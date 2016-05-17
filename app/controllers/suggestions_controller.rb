f2d2ac0e69992e04fb8c6454b706c7acb926d915
Valerie <val.conklin@gmail.com>
2016-05-17 16:35:24 -0700

    Suggestions index now has searchbar which reloads the same page with the matching suggestion resource

--- app/controllers/suggestions_controller.rb
+++ app/controllers/suggestions_controller.rb
@@ -1,6 +1,15 @@
+require 'TunesTakeoutWrapper'
+
 class SuggestionsController < ApplicationController
 
   def index
-    
+    if params[:query]
+      @suggestion = TunesTakeoutWrapper.find(params[:query])
+      # @restaurant = Restaurant.find_by(business_id: @suggestion.food_id)
+      @music = Spotify_Item.find_by(item_id: @suggestion.music_id)
+    end
   end
+
+  # def show
+  # end
 end
