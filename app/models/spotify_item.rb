f8a4172391d0e3e9eef85cc61b11849a2cb7be71
Valerie <val.conklin@gmail.com>
2016-05-17 10:46:53 -0700

    Started beginning of wrapper for spotify item

--- app/models/spotify_item.rb
+++ app/models/spotify_item.rb
@@ -1,5 +1,20 @@
 require 'httparty'
 
 class SpotifyItem
+  BASE_URL = "https://api.spotify.com/v1/"
 
+  def initialize(data)
+    @item_id = data["item_id"]
+    @type = data["type"]
+    @name = data["name"]
+    @url = data["url"]
+    @image_url = data["image_url"]
+  end
+
+  ## write this once you know which type of music you need to pull in
+  # def self.find(id)
+  #   data = HTTParty.get(BASE_URL + "artists/#{id}").parsed_response
+  #
+  #   self.new(data)
+  # end
 end
