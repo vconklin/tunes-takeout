0d13701479f8bbd8e78c81072971793b00b5ae0d
Valerie <val.conklin@gmail.com>
2016-05-19 11:19:01 -0700

    Added find_or_create_from_omniauth class, largely copypaste from kari so trying now to decipher it and figure out why it's not working

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,20 @@ class User < ActiveRecord::Base
 
   def self.find_or_create_from_omniauth(auth_hash)
     # Find or create a user
+    user = User.find_by(uid: auth_hash["uid"], provider: auth_hash["provider"])
+    if !user.nil?
+      #   return user that was found
+      return user
+    else
+      #   no user found, do something here
+      user = User.new(uid: auth_hash["uid"], provider: auth_hash["provider"], name: auth_hash["info"]["name"]
+
+      if user.save
+        return user
+      else
+        return nil
+      end
+    end
   end
+
 end
