31820b8e958b94f3d9370b79420f17a472a99145
Valerie <val.conklin@gmail.com>
2016-05-18 23:02:35 -0700

    Added some more super basic tests for spotify_item_test

--- test/models/spotify_item_test.rb
+++ test/models/spotify_item_test.rb
@@ -10,23 +10,70 @@ class SpotifyItemTest < ActiveSupport::TestCase
 
       describe "track" do
         before do
-          @track = SpotifyItem.find_by(ID_GOES_HERE, "track")
+          # fake tales of san francisco
+          @track = SpotifyItem.find_by("3sF1JaoS1P9KqE0NDGANZt", "track")
         end
 
         it "has an id", :vcr do
           refute @track.id.nil?
         end
+
+        it "has a type", :vcr do
+          refute @track.type.nil?
+        end
+
+        it "has a name", :vcr do
+          refute @track.name.nil?
+        end
+
+        it "has an image", :vcr do
+          refute @track.image.nil?
+        end
       end
 
       describe "artist" do
         before do
-          @artist = SpotifyItem.find_by(ID_GOES_HERE, "artist")
+          # arctic monkeys (as of 5/18/16)
+          @artist = SpotifyItem.find_by("7Ln80lUS6He07XvHI8qqHH", "artist")
+        end
+
+        it "has an id", :vcr do
+          refute @artist.id.nil?
+        end
+
+        it "has a type", :vcr do
+          refute @artist.type.nil?
+        end
+
+        it "has a name", :vcr do
+          refute @artist.name.nil?
+        end
+
+        it "has an image", :vcr do
+          refute @artist.image.nil?
         end
       end
 
       describe "album" do
         before do
-          @album = SpotifyItem.find_by(ID_GOES_HERE, "album")
+          # five minutes with arctic monkeys
+          @album = SpotifyItem.find_by("2MRFr0bOzV9OgVdK6gJXkZ", "album")
+        end
+
+        it "has an id", :vcr do
+          refute @album.id.nil?
+        end
+
+        it "has a type", :vcr do
+          refute @album.type.nil?
+        end
+
+        it "has a name", :vcr do
+          refute @album.name.nil?
+        end
+
+        it "has an image", :vcr do
+          refute @album.image.nil?
         end
       end
 
