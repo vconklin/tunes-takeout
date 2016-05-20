1a3c33f976099981b454ae6caef5b40db7dc4c53
Valerie <val.conklin@gmail.com>
2016-05-19 17:07:49 -0700

    Set up yelp wrapper

--- app/controllers/suggestions_controller.rb
+++ app/controllers/suggestions_controller.rb
@@ -5,7 +5,7 @@ class SuggestionsController < ApplicationController
   def index
     if params[:query]
       @suggestion = TunesTakeoutWrapper.find(params[:query])
-      # @restaurant = Restaurant.find_by(business_id: @suggestion.food_id)
+      @restaurant = Restaurant.find(@suggestion.food_id)
       @music = SpotifyItem.find_by(@suggestion.music_id, @suggestion.music_type)
     end
   end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,22 +1,20 @@
-require 'httparty'
+require 'yelp'
 
 class Restaurant
-  BASE_URL = "https://api.yelp.com/v2/"
+  attr_reader :business_id, :name, :url, :image_url, :phone, :rating
 
   def initialize(data)
-    @business_id = data["business_id"]
-    @name = data["name"]
-    @url = data["url"]
-    @image_url = data["image_url"]
-    @phone = data["phone"]
-    @rating = data["rating"]
+    @business_id = data.id
+    @name = data.name
+    @url = data.url
+    @image_url = data.image_url
+    @phone = data.phone
+    @rating = data.rating
   end
 
-  ## work on this when you know how to use auth key stuff
-  # def self.find(id)
-  #   data = HTTParty.get(BASE_URL + "business/#{id}").parsed_response
-  #
-  #   self.new(data)
-  # end
+  def self.find(id)
+    data = Yelp.client.business(id)
+    self.new(data.business)
+  end
 
 end
