d5738fcc7ab3fb940567818bead50ed248abd7ed
Valerie <val.conklin@gmail.com>
2016-05-20 11:30:50 -0700

    Address formatted with newlines

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,7 +1,7 @@
 require 'yelp'
 
 class Restaurant
-  attr_reader :business_id, :name, :url, :image_url, :phone, :rating
+  attr_reader :business_id, :name, :url, :image_url, :phone, :rating, :address
 
   def initialize(data)
     @business_id = data.id
@@ -10,6 +10,7 @@ class Restaurant
     @image_url = data.image_url
     @phone = data.phone
     @rating = data.rating
+    @address = data.location.display_address
   end
 
   def self.find(id)
